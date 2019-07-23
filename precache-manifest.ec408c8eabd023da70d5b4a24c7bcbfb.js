e1e3e24c55d065d12a1eaa98999dd5ec0618ad79
Bianka Korban <bianka.korban@gmail.com>
2019-07-23 17:33:15 +1000

    Updates

---
+++ precache-manifest.ec408c8eabd023da70d5b4a24c7bcbfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2350fe62b7f88a69b4d4c6ed913642d5",
+    "url": "/react-todoapp-with-route/index.html"
+  },
+  {
+    "revision": "a6f2b30bb9777961b4c8",
+    "url": "/react-todoapp-with-route/static/css/main.e58dbefb.chunk.css"
+  },
+  {
+    "revision": "53982020dc87d7ea4f72",
+    "url": "/react-todoapp-with-route/static/js/2.ba9a74be.chunk.js"
+  },
+  {
+    "revision": "a6f2b30bb9777961b4c8",
+    "url": "/react-todoapp-with-route/static/js/main.02c11519.chunk.js"
+  },
+  {
+    "revision": "3e1270a814c8dc767592",
+    "url": "/react-todoapp-with-route/static/js/runtime~main.0f37e290.js"
+  }
+]);
